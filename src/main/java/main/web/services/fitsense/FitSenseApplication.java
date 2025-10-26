807d28ed730f45bd136ef813243f9bb79bec75d5
Fiorella <U202120418@upc.edu.pe>
2025-10-26 18:11:41 -0500

    first commit

---
+++ src/main/java/main/web/services/fitsense/FitSenseApplication.java
@@ -0,0 +1,13 @@
+package main.web.services.fitsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitSenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitSenseApplication.class, args);
+	}
+
+}

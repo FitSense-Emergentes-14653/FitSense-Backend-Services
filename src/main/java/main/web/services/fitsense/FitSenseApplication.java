c68acf8439d7586d6e8a7b654f4d3dfc8d1a4858
Fiorella <U202120418@upc.edu.pe>
2025-10-26 18:14:36 -0500

    feat: added Create User (Sign-Up) via API

--- src/main/java/main/web/services/fitsense/FitSenseApplication.java
+++ src/main/java/main/web/services/fitsense/FitSenseApplication.java
@@ -2,8 +2,10 @@ package main.web.services.fitsense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FitSenseApplication {
 
 	public static void main(String[] args) {
